496232e7fa7d8a32193d1efe668caab202ff3fd3
hotsixman <hotsixman05@gmail.com>
2025-07-21 17:43:31 +0900

    feat: 카드 리스트 가져오는 함수

---
+++ src/class/DonderHiroba.ts
@@ -0,0 +1,325 @@
+import { CardData } from "../types/types";
+import { checkNamcoLogin, Const, createHeader, HirobaError, sanitizeHTML, parseHTML, isBrowser } from "../util";
+import setCookieParser from 'set-cookie-parser';
+
+export class DonderHiroba {
+
+}
+
+export namespace DonderHiroba {
+    export namespace request {
+        export async function cardList(data?: { token?: string }) {
+            const { token } = data ?? {};
+            try {
+                var response = await fetch('https://donderhiroba.jp/login_select.php', {
+                    method: 'get',
+                    headers: createHeader(token ? `_token_v2=${token}` : undefined),
+                    redirect: 'manual'
+                });
+            }
+            catch (err) {
+                if (err instanceof Response) {
+                    throw new HirobaError('CANNOT_CONNECT', err);
+                }
+                else {
+                    throw new HirobaError('CANNOT_CONNECT');
+                }
+            }
+
+            const { logined, error } = checkNamcoLogin(response);
+            if (!logined) throw error;
+
+            const html = await response.text();
+            if (isBrowser()) {
+                return html;
+            }
+            else {
+                return sanitizeHTML(html);
+            }
+        };
+
+        export async function clearData(data?: { token?: string }): Promise<string[]>;
+        export async function clearData(data?: { token?: string, genre: keyof typeof Const.genre }): Promise<string>;
+        export async function clearData(data?: { token?: string, genre?: keyof typeof Const.genre }) {
+            const genre = data?.genre ?? undefined;
+            const token = data?.token ?? undefined;
+            if (genre) {
+                try {
+                    var response = await fetch(`https://donderhiroba.jp/score_list.php?genre=${Const.genre[genre]}`, {
+                        headers: createHeader(token ? `_token_v2=${token}` : undefined),
+                        redirect: 'manual'
+                    })
+                }
+                catch (err) {
+                    if (err instanceof Response) {
+                        throw new HirobaError('CANNOT_CONNECT', err);
+                    }
+                    else {
+                        throw new HirobaError('CANNOT_CONNECT');
+                    }
+                }
+
+                const { logined, error } = checkNamcoLogin(response);
+                if (!logined) throw error;
+
+                return await response.text();
+            }
+            else {
+                const genres = [1, 2, 3, 4, 5, 6, 7, 8];
+                const datas: string[] = [];
+                for (const genre of genres) {
+                    try {
+                        var response = await fetch(`https://donderhiroba.jp/score_list.php?genre=${genre}`, {
+                            headers: createHeader(token ? `_token_v2=${token}` : undefined),
+                            redirect: 'manual'
+                        })
+                    }
+                    catch (err) {
+                        if (err instanceof Response) {
+                            throw new HirobaError('CANNOT_CONNECT', err);
+                        }
+                        else {
+                            throw new HirobaError('CANNOT_CONNECT');
+                        }
+                    }
+
+                    const { logined, error } = checkNamcoLogin(response);
+                    if (!logined) throw error;
+
+                    datas.push(await response.text());
+                };
+
+                return datas;
+            }
+        };
+    }
+
+    export namespace parse {
+        export function cardList(html: string) {
+            const dom = parseHTML(html);
+
+            const cardList: CardData[] = [];
+            dom.querySelectorAll('.cardSelect').forEach((el) => {
+                const taikoNumber = el.querySelector('div#mydon_area > div:nth-child(2) > p')?.textContent?.replace('太鼓番: ', '')?.trim();
+                const nickname = el.querySelector('div#mydon_area > div:nth-child(3)')?.textContent?.replaceAll('\n', '')?.replaceAll('\t', '');
+                const myDon = el.querySelector('img')?.getAttribute('src');
+
+                if (!taikoNumber || !nickname || !myDon) return;
+                cardList.push({
+                    taikoNumber,
+                    nickname,
+                    myDon
+                })
+            });
+
+            return cardList;
+        }
+    }
+
+    export namespace func {
+        /**
+         * 세션 토큰을 가져옵니다. 세션 토큰은 사용자를 구분하는 데 사용합니다.
+         * @param email 
+         * @param password 
+         * @returns 
+         */
+        export async function getSessionToken(email: string, password: string) {
+            /*
+            첫 번째 요청
+            200 응답
+            */
+            let response: Response;
+            try {
+                const data = {
+                    client_id: 'nbgi_taiko',
+                    redirect_uri: 'https://www.bandainamcoid.com/v2/oauth2/auth?back=v3&client_id=nbgi_taiko&scope=JpGroupAll&redirect_uri=https%3A%2F%2Fdonderhiroba.jp%2Flogin_process.php%3Finvite_code%3D%26abs_back_url%3D%26location_code%3D&text=',
+                    customize_id: '',
+                    login_id: email,
+                    password: password,
+                    shortcut: 0,
+                    retention: 0,
+                    language: 'ko',
+                    cookie: '{"language":"ko"}',
+                    prompt: 'login',
+                };
+
+                const params = new URLSearchParams();
+                for (const [key, value] of Object.entries(data)) {
+                    params.append(key, `${value}`);
+                }
+
+                response = await fetch('https://account-api.bandainamcoid.com/v3/login/idpw', {
+                    method: 'post',
+                    headers: { 'Content-Type': 'application/x-www-form-urlencoded', ...createHeader() },
+                    body: params
+                });
+
+                if (response.status !== 200) {
+                    throw response;
+                }
+            }
+            catch (err) {
+                if (err instanceof Response) {
+                    throw new HirobaError('CANNOT_CONNECT', err);
+                }
+                else {
+                    throw new HirobaError('CANNOT_CONNECT');
+                }
+            }
+
+            /*
+            두 번째 요청
+            302 응답
+            */
+            try {
+                const data = await response.json();
+                if (!data.redirect) {
+                    throw new HirobaError("INVALID_ID_PASSWORD");
+                }
+
+                let cookieString = '';
+                const cookies = setCookieParser(response.headers.getSetCookie())
+                for (const cookie of cookies) {
+                    if (cookie.domain === '.bandainamcoid.com' && cookie.value !== undefined) {
+                        cookieString += cookie.name + '=' + cookie.value + ';';
+                    }
+                } response = await fetch(data.redirect, {
+                    headers: createHeader(cookieString),
+                    redirect: 'manual'
+                });
+            }
+            catch (err) {
+                if (err instanceof Response) {
+                    throw new HirobaError("CANNOT_CONNECT", err)
+                }
+                else if (err instanceof HirobaError) {
+                    throw err
+                }
+                else {
+                    throw new HirobaError('CANNOT_CONNECT');
+                }
+            }
+
+            /*
+            세 번째 요청
+            302
+            */
+            try {
+                response = await fetch(response.headers.get('location') as string, {
+                    headers: createHeader(),
+                    redirect: 'manual'
+                });
+
+                if (response.status !== 302) {
+                    throw response;
+                }
+            }
+            catch (err) {
+                if (err instanceof Response) {
+                    throw new HirobaError('CANNOT_CONNECT', err);
+                }
+                else {
+                    throw new HirobaError('CANNOT_CONNECT');
+                }
+            }
+
+            /*
+            네 번째 요청
+            200
+            */
+            try {
+                response = await fetch(response.headers.get('location') as string, {
+                    headers: createHeader(response.headers.getSetCookie()[2].split(';')[0])
+                });
+                const token = setCookieParser(response.headers.getSetCookie()).find(e => e.name === '_token_v2')?.value as string;
+                if (!token) {
+                    throw response;
+                }
+                return token;
+            }
+            catch (err) {
+                if (err instanceof Response) {
+                    throw new HirobaError('CANNOT_CONNECT', err);
+                }
+                else {
+                    throw new HirobaError('CANNOT_CONNECT');
+                }
+            }
+        }
+
+        /**
+         * 계정에 등록된 카드 리스트를 가져옵니다.
+         * 이 함수를 사용하면 카드 로그인이 풀립니다.
+         * @param token 
+         * @returns 
+         */
+        export async function getCardList(data?: { token?: string }) {
+            const html = await request.cardList(data);
+            return parse.cardList(html);
+        }
+
+        /**
+         * 특정 북번호를 가진 카드로 로그인합니다.
+         * @param data 
+         */
+        export async function cardLogin(data: { token?: string, taikoNumber: string, cardList?: CardData[]}) {
+            const { token, taikoNumber } = data;
+
+            const cardList = data.cardList ?? await getCardList(data);
+            const matchedCardIndex = cardList.findIndex(card => card.taikoNumber === taikoNumber);
+
+            if (matchedCardIndex === -1) {
+                throw new HirobaError('NO_MATCHED_CARD');
+            }
+
+            let response: Response;
+            // 첫 번째 요청
+            // 302 응답
+            try {
+                const params = new URLSearchParams();
+                params.set('id_pos', `${matchedCardIndex + 1}`);
+                params.set('mode', 'exec');
+
+                response = await fetch('https://donderhiroba.jp/login_select.php', {
+                    method: 'post',
+                    headers: createHeader(token ? `_token_v2=${token}` : undefined),
+                    redirect: 'manual',
+                    body: params
+                });
+
+                if (response.status !== 302) {
+                    throw response;
+                }
+            }
+            catch (err) {
+                if (err instanceof Response) {
+                    throw new HirobaError('CANNOT_CONNECT', err);
+                }
+                else {
+                    throw new HirobaError('CANNOT_CONNECT');
+                }
+            }
+
+            // 두 번째 요청
+            // 200 응답
+            try{
+                response = await fetch(response.headers.get('location') as string, {
+                    method: 'get',
+                    headers: createHeader(token ? `_token_v2=${token}` : undefined)
+                });
+
+                if(response.status !== 200) throw response;
+            }
+            catch(err){
+                if (err instanceof Response) {
+                    throw new HirobaError('CANNOT_CONNECT', err);
+                }
+                else {
+                    throw new HirobaError('CANNOT_CONNECT');
+                }
+            };
+
+            return cardList[matchedCardIndex];
+        }
+    }
+}
---
+++ src/util.ts
@@ -0,0 +1,122 @@
+import { load } from "cheerio";
+import { parse as nodeHtmlParser, type HTMLElement } from 'node-html-parser';
+
+export function createHeader(cookie?: string) {
+    let headers;
+    if (cookie) {
+        headers = ({
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Accept-Language': 'ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7',
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
+            cookie
+        })
+    }
+    else {
+        headers = ({
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Accept-Language': 'ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7',
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
+        })
+    }
+
+    return new Headers(headers);
+}
+
+export class HirobaError extends Error {
+    code: string;
+    response?: Response;
+
+    constructor(code: HirobaErrorCode, response?: Response) {
+        super(code);
+        this.code = code;
+        this.response = response;
+    }
+}
+
+export type HirobaErrorCode = 'NOT_LOGINED' | 'NOT_NAMCO_LOGINED' | 'CANNOT_CONNECT' | 'INVALID_ID_PASSWORD' | 'NO_MATCHED_CARD' | 'UNKNOWN_ERROR';
+
+/**
+ * 반다이 남코 아이디로 로그인 되어 있는 지 체크합니다.
+ */
+export function checkNamcoLogin(response: Response) {
+    // 200 응답이 아닐 때
+    if (response.status !== 200) {
+        return {
+            logined: false,
+            error: new HirobaError('NOT_LOGINED', response)
+        }
+    }
+
+    // origin이 동더히로바가 아닐 때
+    if (new URL(response.url).origin !== 'https://donderhiroba.jp') {
+        return {
+            logined: false,
+            error: new HirobaError('NOT_LOGINED', response)
+        }
+    }
+
+    return {
+        logined: true,
+        error: null
+    }
+}
+
+/**
+ * 카드 로그인이 되어 있는 지 체크합니다.
+ */
+export function checkCardLogin(response: Response) {
+    // 200 응답이 아닐 때
+    if (response.status !== 200) {
+        return {
+            logined: false,
+            error: new HirobaError('NOT_LOGINED', response)
+        }
+    }
+
+    // origin이 동더히로바가 아닐 때
+    if (new URL(response.url).origin !== 'https://donderhiroba.jp') {
+        return {
+            logined: false,
+            error: new HirobaError('NOT_LOGINED', response)
+        }
+    }
+
+    return {
+        logined: true,
+        error: null
+    }
+}
+
+export function isBrowser(){
+    return typeof (window) !== "undefined";
+}
+
+export function parseHTML(html: string) {
+    if (isBrowser()) { //browser
+        return new DOMParser().parseFromString(html, 'text/html') as unknown as HTMLElement;
+    }
+    else { //nodejs
+        return nodeHtmlParser(html) as HTMLElement;
+    }
+}
+export function sanitizeHTML(html: string) {
+    let $ = load(html);
+    return $.html();
+}
+
+export namespace Const {
+    export const genre = {
+        pops: 1,
+        anime: 2,
+        kids: 3,
+        vocaloid: 4,
+        game: 5,
+        namco: 6,
+        variety: 7,
+        classic: 8
+    } as const;
+}
